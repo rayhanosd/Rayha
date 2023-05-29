a54f49be7d8788d0376b9b6a8229c0f185b8f105
Saif Islam Rayhan <134764307+rayhanosd@users.noreply.github.com>
2023-05-29 13:20:09 +0600

    else if

---
+++ jgjgfgj.c
@@ -0,0 +1,19 @@
+    #include<stdio.h>
+    int main (){
+    int n = 1;
+    if(n<5) {
+            printf ("the number of negative\n");
+
+
+    }
+    else if (n>5){
+        printf ("the number of positive\n");
+    }
+else if (n == 0)
+
+{
+    Printf ("the number of zero!\n");
+}
+
+    return 0;
+    }
